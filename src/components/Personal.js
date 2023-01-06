1fc06763b4f0dc3cbe07ee1d0b5758d35b0a062b
Justin Strang <Justinstrang100@gmail.com>
2023-01-06 15:55:47 -0500

    inputs are required

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -52,26 +52,24 @@ class Personal extends Component {
                     <form onSubmit={this.onSubmitHandler}>
                         <label htmlFor='firstNameInput'>
                             First Name:
-                            <input type='text' name='firstName' onChange={this.handleChange} value={firstName} id='firstNameInput'/>
+                            <input type='text' name='firstName' onChange={this.handleChange} value={firstName} id='firstNameInput' required/>
                         </label>
     
                         <label htmlFor='lastNameInput'>
                             Last Name:
-                            <input type='text' name='lastName' onChange={this.handleChange} value={lastName} id='lastNameInput'/>
+                            <input type='text' name='lastName' onChange={this.handleChange} value={lastName} id='lastNameInput' required/>
                         </label>
     
                         <label htmlFor='emailInput'>
                             Email:
-                            <input type='email' name='email' onChange={this.handleChange} value={email} id='emailInput'/>
+                            <input type='email' name='email' onChange={this.handleChange} value={email} id='emailInput' required/>
                         </label>
     
                         <label htmlFor='phoneInput'>
                             Phone:
-                            <input type='tel' name='phone' onChange={this.handleChange} value={phone} id='phoneInput' pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder='000-000-0000'/>
+                            <input type='tel' name='phone' onChange={this.handleChange} value={phone} id='phoneInput' pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder='000-000-0000' required/>
                         </label>
-    
-                        <input type='button' value='Edit'/>
-    
+        
                         <input type='button' value='Delete' onClick={this.deleteData}/>
                         
                         <input type='submit' value='Submit'/>
@@ -82,20 +80,20 @@ class Personal extends Component {
         else {
             return (
                 <div>
-                            <h3>FirstName:</h3>
-                            <p>{firstName}</p>
-                            <h3>LastName:</h3>
-                            <p>{lastName}</p>
-                            <h3>Phone:</h3>
-                            <p>{phone}</p>
-                            <h3>Email:</h3>
-                            <p>{email}</p>
-                            <button onClick={() => {
-                                this.setState({
-                                    edit: !this.edit
-                                })
-                            }}>Edit Personal</button>
-                        </div>
+                    <h3>FirstName:</h3>
+                    <p>{firstName}</p>
+                    <h3>LastName:</h3>
+                    <p>{lastName}</p>
+                    <h3>Phone:</h3>
+                    <p>{phone}</p>
+                    <h3>Email:</h3>
+                    <p>{email}</p>
+                    <button onClick={() => {
+                        this.setState({
+                            edit: !this.edit
+                        })
+                    }}>Edit Personal</button>
+                </div>
             )
         }
         
